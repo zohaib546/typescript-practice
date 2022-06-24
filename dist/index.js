9a9e7ba0b0699008e20d82f48a892f1b372d6c13
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-06-24 20:01:27 +0500

    fundamentals watched

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hello world");
+var Size;
+(function (Size) {
+    Size[Size["small"] = 1] = "small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
