2e2b2e831ddda621f929f9987d400e28682b7d3c
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-07-01 19:47:14 +0500

    working on classes

--- src/index.ts
+++ src/index.ts
@@ -49,3 +49,52 @@
 // };
 
 // // employee.id = 0;
+
+// LITERAL TYPES (exact, specific)
+// type Quantity = 50 | 100;
+// let quantity: Quantity = 50;
+
+// OPTIONAL CHAINING
+// type Customer = {
+//   birthday?: Date;
+// };
+
+// function getCustomer(id: number): Customer | null | undefined {
+//   return id === 0 ? null : { birthday: new Date() };
+// }
+
+// let customer = getCustomer(1);
+// console.log(customer?.birthday?.getFullYear());
+
+// NULLISH COAELSCING
+// let speed: number | null = null;
+
+// let ride = {
+//   speed: speed ?? 30,
+// };
+
+// TYPE ASSERTIONS
+// let phone = document.getElementById("phone") as HTMLInputElement;
+// // let phone = <HTMLInputElement>document.getElementById("phone");
+
+// phone.value;
+
+// UNKNOWN TYPE
+// function render(document: unknown) {
+//   // Narrowing
+//   //   if (typeof document === "string") {
+//   //     document.toUpperCase();
+//   //   }
+//   document.fly();
+//   document.crawl();
+// }
+
+// NEVER TYPE
+// function processEvents(): never {
+//   while (true) {
+//     // Read a message from a queue
+//   }
+// }
+
+// processEvents();
+// console.log("will never executes");
