350e310a7900513193b3d8e13e9b55aac2202025
Zohaib Ashraf <zohaibashraf546@gmail.com>
2022-08-29 18:12:11 +0500

    revised till generics

--- src/generics.ts
+++ src/generics.ts
@@ -1,3 +1,20 @@
+// EXAMPLE:
+// class KeyValuePair {
+//   constructor(public key: number, public value: string) {}
+// }
+
+// class StringKeyValuePair {
+//   constructor(public key: string, public value: string) {}
+// }
+
+// let pair = new KeyValuePair(1, 'Orange');
+// let newpair = new StringKeyValuePair("1", "Apple");
+
+// This is an issue for each change we have to make a new type of constant or class
+// so we need a solution which reduces redundancy of types and constants
+// that's where generic helps us see below example:
+// with generics we got type-safety, intellisense, and reduced redundancy
+
 // GENERIC CLASSES:
 // class KeyValuePair<K, V> {
 //   constructor(public key: K, public value: V) {}
@@ -113,19 +130,57 @@
 // }
 
 // KEYOF OPERATOR
+// interface Product {
+//   name: string;
+//   price: number;
+// }
+
+// class Store<T> {
+//   protected _objects: T[] = [];
+
+//   add(obj: T): void {
+//     this._objects.push(obj);
+//   }
+
+//   // if 'T' is product
+//   // keyof T => 'name' | 'price'
+//   find(property: keyof T, value: unknown): T | undefined {
+//     return this._objects.find((obj) => obj[property] === value);
+//   }
+// }
+
+// let store = new Store<Product>();
+// store.add({ name: "a", price: 1 });
+// store.find("name", "a");
+// store.find("price", 1);
+// store.find("nonExistingProperty", 10); // here 'nonExistingProperty' no exists in 'Product'
+
+// TYPE MAPPING
+// sometime we need a type based on another type
 interface Product {
   name: string;
   price: number;
 }
 
-class Store<T> {
-  protected _objects: T[] = [];
+// instead of hardcoding 'Product' properties
+// we use =>
+// Index signature
+// keyof
+type ReadOnly<T> = {
+  readonly [Key in keyof T]: T[Key];
+};
 
-  add(obj: T): void {
-    this._objects.push(obj);
-  }
+type Optional<T> = {
+  [Key in keyof T]?: T[Key];
+};
 
-  find(property: string, value: unknown): T | undefined {
-    return this._objects.find((obj) => obj[property] === value);
-  }
-}
+type Nullable<T> = {
+  [Key in keyof T]: T[Key] | null;
+};
+
+let product: ReadOnly<Product> = {
+  name: "a",
+  price: 10,
+};
+
+// product.name = "b"; // you cant modify readonly properties
